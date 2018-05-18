5a905ea3ac9b8dedd1837f3f1338a9516362c4ae
Fabian Meyer <fabian.niklas.meyer@rwth-aachen.de>
2018-05-18 20:21:02 +0200

    Spaltenbezeichnung urkunden + Sex und Raw

--- src/Handler/Urkunden.hs
+++ src/Handler/Urkunden.hs
@@ -14,10 +14,13 @@ getUrkundenR :: Handler TypedContent
 getUrkundenR = do
                  liftersFromDB <- getLiftersFromDB
                  let lifters = liftersWithPlacings liftersFromDB
-                 let lifterCSV =  T.concat $ P.map (T.concat .
+                 let lifterCSV =  (++) identifiers $ T.concat $ P.map (T.concat .
                                   map
                                   (\(pl,l) -> T.intercalate "," [lifterName l, lifterClub l, pack (show $ lifterAgeclass l),
                                               pack (show $ lifterWeightclass l), showPlacing l pl,
-                                              showTotal l, calcWilks l] ++ "\n"))
+                                              showTotal l, calcWilks l, pack $ show $ lifterRaw l, pack $ show $ lifterSex l] ++ "\n"))
                                   lifters -- :: Text
                  return $ TypedContent "text/csv" $ toContent lifterCSV--(T.pack $ show $ lifterCSV)
+
+              where
+                identifiers = "Name,Club,Ageclass,Weightclass,Placing,Total,Wilks,Raw,Sex\n"
